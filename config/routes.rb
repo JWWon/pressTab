deeca72d71b140a67fc76484bd19201301ea285d
JiwoonWon <wonjiwn@naver.com>
2016-12-03 00:51:11 +0900

    add rule controller!

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root "landing#index"
 
   get '/' => "landing#index"
+  get '/rule' => "landing#rule"
   post '/write' => "landing#write"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
